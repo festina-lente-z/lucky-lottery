d569cc76a0efbf662a633fe517ef69354597f7ce
BellaZZZZZ <bellazhao_321@qq.com>
2021-09-06 23:10:53 +0800

    增加测试数据

--- src/components/Lottery.js
+++ src/components/Lottery.js
@@ -8,16 +8,63 @@ const getStorageMask = () => {
   }
   return mask
 }
+
+const mockData = [
+  {
+    mask: "no-mask",
+    id: 0
+  },
+  {
+    mask: "no-mask",
+    id: 1
+  },
+  {
+    mask: "no-mask",
+    id: 2
+  },
+  {
+    mask: "no-mask",
+    id: 3
+  }, 
+  {
+    mask: "no-mask",
+    id: 4
+  },
+  {
+    mask: "no-mask",
+    id: 5
+  },
+  {
+    mask: "no-mask",
+    id: 6
+  },
+  {
+    mask: "no-mask",
+    id: 7
+  }
+];
+
 const Lottery = () => {
-  const numbers = [0,1,2,3,4,5,6,7];
-  const [ mask, setMask ] = useState('no-mask');
+  // 当前转动的次数
+  let currentIndex = 0;
+  let count = 0;
+  let speed = 50;
+  const [ mask, setMask ] = useState({mask:'no-mask',id:0});
   const handleLotteryStart = () => {
-    mask === 'no-mask' ? setMask('is-mask') : setMask('no-mask')
+    // mask === 'no-mask' ? setMask('is-mask') : setMask('no-mask')
+    const currentItem = currentIndex % 8;
+    currentIndex += 1;
+    mockData.forEach(item => {
+      const {id, mask} = item;
+      mask = id == currentItem ? 'is-mask' : 'no-mask'
+    });
+
   }
   return (
     <div className='wrapper'>
-      {numbers.map((item,index)=>{
-        return <LotteryItem mask={mask} key={index} area={'item'+item} prize={item}/>
+      {mockData.map((item,index)=>{
+        const {mask, id} = item
+        return <LotteryItem mask={mask} key={index} area={'item'+id} prize={id}/>
       })}
       <button className='btn-child item8' onClick={handleLotteryStart}>抽奖</button>
     </div>
